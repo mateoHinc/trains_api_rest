c214ef0d605b218154e4c3cc2ad2a630137340b3
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2026-03-15 12:50:24 -0500

    Crear repositorio para validar id de rutas, fecha de viaje y número de asiento

--- trains_api_rest/src/main/java/com/company/trains_api_rest/repository/TicketRepository.java
+++ trains_api_rest/src/main/java/com/company/trains_api_rest/repository/TicketRepository.java
@@ -1,10 +1,28 @@
 package com.company.trains_api_rest.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.company.trains_api_rest.model.Ticket;
+import com.company.trains_api_rest.model.TicketStatus;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long>, JpaSpecificationExecutor<Ticket>{
+
+    boolean existsByRouteIdAndTravelDateAndSeatNumberAndStatusNot(
+        Long routeId,
+        LocalDate traveDate,
+        String seatNumber,
+        TicketStatus status
+    );
+
+    boolean existsByRouteIdAndTravelDateAndSeatNumberAndStatusNotANdIdNot(
+        Long routeId,
+        LocalDate traveDate,
+        String seatNumber,
+        TicketStatus status,
+        Long id
+    );
 
 }
