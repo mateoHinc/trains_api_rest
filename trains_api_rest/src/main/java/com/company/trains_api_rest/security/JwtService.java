c164d9ea0ecfb3b0d583815989ccb2cdc287f982
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2026-03-16 17:10:56 -0500

    Crear seguridad para jwt

---
+++ trains_api_rest/src/main/java/com/company/trains_api_rest/security/JwtService.java
@@ -0,0 +1,60 @@
+package com.company.trains_api_rest.security;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    
+    private static final String SECRET_KEY = "123456789";
+
+    private String generateToken(String email, String role) {
+        return Jwts.builder().setSubject(email)
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+
+    private boolean isTokenValid(String token, String email) {
+        final String username = extractUsername(token);
+        return username.equals(email) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(Base64.getEncoder().encodeToString(SECRET_KEY.getBytes()));
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
